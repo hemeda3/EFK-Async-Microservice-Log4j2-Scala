414f0df6c9415bc811a06fa7ffd5d7029be786cb
Mohammed <ahmohammed@canada.root.corp>
2018-05-29 01:05:39 -0400

    new service

---
+++ loadtest/src/test/scala/gatling/simulations/FooSimulation.scala
@@ -0,0 +1,16 @@
+package gatling.simulations
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class FooSimulation extends Simulation {
+  val httpProtocol = http
+    .baseURL(s"http://localhost")
+  val scn = scenario("foo load test")
+    .exec(http("Mark").get("/foo"))
+  setUp(
+    scn.inject(
+      atOnceUsers(1)
+    )
+  ).protocols(httpProtocol)
+}
